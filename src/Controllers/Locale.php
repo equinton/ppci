da1180836c9b18813c319e52523d2fac110b327a
Eric Quinton <eric.quinton@inrae.fr>
2024-06-04 10:51:29 +0200

    :feat locale: add translations

--- src/Controllers/Locale.php
+++ src/Controllers/Locale.php
@@ -1,19 +1,23 @@
 <?php
+
 namespace Ppci\Controllers;
 
 use Ppci\Libraries\Locale as LibrariesLocale;
 
 class Locale extends PpciController
 {
-    function Index($locale = "") {
+    function Index($locale = "")
+    {
         if (empty($locale) && isset($_REQUEST["locale"])) {
             $locale = $_REQUEST["locale"];
+        }
+        if (!empty($locale)) {
             $localeLib = new LibrariesLocale();
             $localeLib->setLocale($locale);
-            if (isset($_SESSION["menu"]) ){
-                unset ($_SESSION["menu"]);
+            if (isset($_SESSION["menu"])) {
+                unset($_SESSION["menu"]);
             }
-            return defaultPage();
         }
+        return defaultPage();
     }
 }
